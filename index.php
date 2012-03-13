356df39370b5d903ee3831021061ae1d766ebd58
Geoffroy Thomasson <geoffroy.thomasson@sfr.fr>
2012-03-13 20:50:59 +0100

    Deuxieme depot

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fiche descriptive: Stevia Rebaudiana</title>
+<link href="css/template.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+<body>
+</body>
+</html>
